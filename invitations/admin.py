64a0e89a0929976e7c4286fd0958c588647bf655
Sunah Suh <sunah@floship.com>
2015-06-13 15:07:23 -0500

    Register models in admin

--- invitations/admin.py
+++ invitations/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Invitation, Guest
 
-# Register your models here.
+
+class GuestInline(admin.TabularInline):
+    model = Guest
+    extra = 2
+
+
+@admin.register(Invitation)
+class InvitationAdmin(admin.ModelAdmin):
+    inlines = (GuestInline,)
+
+
+@admin.register(Guest)
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'rsvp')
