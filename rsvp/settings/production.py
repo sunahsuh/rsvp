fd5e8dd49dc1092974b02eaff7186cb31350ec1e
Sunah Suh <sunah@floship.com>
2015-06-22 21:06:05 -0500

    Use normal static files

--- rsvp/settings/production.py
+++ rsvp/settings/production.py
@@ -10,4 +10,13 @@ X_FRAME_OPTIONS = 'ALLOW-FROM sunahandlaura.com'
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+)
